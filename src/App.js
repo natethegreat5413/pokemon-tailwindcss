dd74824b0378ea3ea0783775eb39e678cfe69e92
Nathan Cowley <nathancowley12@gmail.com>
2021-02-23 14:47:58 -0700

    finished the pokemon app with tailwind

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
 import './App.css';
+import { Switch, BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import About from './About';
+import Home from './Home';
+import React, { useEffect, useState, useMemo } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [pokemon, setPokemon] = useState([]);
+	const [filteredPokemon, setFilteredPokemon] = useState([]);
+	const [text, setText] = useState([]);
+	useEffect(() => {
+		fetch('https://pokeapi.co/api/v2/pokemon?offset=0')
+			.then((res) => res.json())
+			.then((data) => {
+				const results = data.results.map((pokemon, idx) => {
+					return { ...pokemon, idx: idx + 1 };
+				});
+				setPokemon({ ...data, results });
+			});
+	}, []);
+
+	useMemo(() => {
+		if (!text) {
+			setFilteredPokemon([]);
+			return;
+		}
+		// This function works by whatever text you type, if the pokemon has that text in the name
+		// then it will be displayed and you can click on it.
+		setFilteredPokemon(() => pokemon.results?.filter((pokemon) => pokemon.name.includes(text)));
+	}, [pokemon.results, text]);
+
+	return (
+		<Router>
+			<div className="p-14">
+				<div className="flex flex-col items-center">
+					<Link to="/">
+						<header className="text-4xl text-yellow-700">Pokemon Picker</header>
+					</Link>
+				</div>
+				<div className="w-full flex justify-center">
+					<input
+						onChange={(event) => setText(event.target.value)}
+						placeholder="Enter Pokemon here"
+						type="text"
+						className="mt-10 p-2 border-blue-500 border-2"
+					/>
+				</div>
+				<Switch>
+					<Route path="/about/:id">
+						<About>hi</About>
+					</Route>
+					<Route path="/">
+						<Home pokemon={filteredPokemon} />
+					</Route>
+				</Switch>
+			</div>
+		</Router>
+	);
 }
 
 export default App;
